f0b669ac5b3198690c8e4de5c0952ee64cbd4a9f
rodrigopinedar <151784710+rodrigopinedar@users.noreply.github.com>
2024-03-14 18:43:37 +0100

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+print("Hello, World")
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+
+
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
